a455e26663af5cc1b4414bae86d8105912592fc4
yusufbesirarslan <yusufbesirarslan@gmail.com>
2026-05-01 17:00:47 +0300

    Add PORT config

--- starter.py
+++ starter.py
@@ -1146,5 +1146,6 @@ def server_error(e):
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all() 
-    app.run(debug=True)
+        db.create_all()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
