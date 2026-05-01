d06abdff92ac0064a6d7a7ede9899b8bf2bf694c
yusufbesirarslan <yusufbesirarslan@gmail.com>
2026-05-01 17:14:01 +0300

    Fix db init and templates

--- starter.py
+++ starter.py
@@ -1147,6 +1147,7 @@ def server_error(e):
 with app.app_context():
     db.create_all()
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
