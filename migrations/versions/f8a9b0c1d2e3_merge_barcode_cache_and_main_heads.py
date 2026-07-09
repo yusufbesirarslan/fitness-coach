5beedd6db10ca8bfd0e90e7fcb926ab4f989ea87
yusufbesirarslan <yusufbesirarslan@gmail.com>
2026-07-09 20:03:14 +0300

    Fix PR 133 CI failures

---
+++ migrations/versions/f8a9b0c1d2e3_merge_barcode_cache_and_main_heads.py
@@ -0,0 +1,19 @@
+"""Merge barcode cache migration with main migration head.
+
+Revision ID: f8a9b0c1d2e3
+Revises: e7f8a9b0c1d2, c5d6e7f8a9b0
+Create Date: 2026-07-09
+"""
+
+revision = "f8a9b0c1d2e3"
+down_revision = ("e7f8a9b0c1d2", "c5d6e7f8a9b0")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
